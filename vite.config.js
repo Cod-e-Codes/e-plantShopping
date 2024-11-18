425b0ce6329f031da556e1299dda3806a4087630
Cody Marsengill <codylmarsengill@gmail.com>
2024-11-18 16:30:24 -0500

    Update config files for deployment to gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/shoppingreact/' : '/', // Use base only in production
+  base: process.env.NODE_ENV === 'production' ? '/e-plantShopping/' : '/',
   plugins: [react()],
   server: {
     host: true, // Allow external devices to access the app
